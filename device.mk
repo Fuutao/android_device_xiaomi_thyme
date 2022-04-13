884786a4731b5d603ae5ac955391a66c6d687fe6
Arian <arian.kulmer@web.de>
2022-04-14 00:12:50 +0200

    thyme: Don't explicitly build fs_config_files
    The fs_config_files for each partition is already added to
    PRODUCT_PACKAGES by base_{product,system,system_ext,vendor}.mk.
    
    Change-Id: Ifbadacd5cb20beca70ed8b3accb3baf86a989c92

--- device.mk
+++ device.mk
@@ -131,10 +131,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
-# AID/fs configs
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # ANT+
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
