c7f19b59a2ab2c7811e1338a4849c207873edd59
Fuutao <fuutaoao@gmail.com>
2024-07-03 23:09:30 +0800

    Fixup! thyme: update-sha1sums: Set up the correct device and vendor

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -20,8 +20,8 @@ import os
 import sys
 from hashlib import sha1
 
-device='sm8550-common'
-vendor='sony'
+device='thyme'
+vendor='xiaomi'
 
 with open('proprietary-files.txt', 'r') as f:
     lines = f.read().splitlines()
